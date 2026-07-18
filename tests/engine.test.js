99709d1466020796ffea6ad0b619cd29b03197d7
BreathPractice <naturalstupid777@gmail.com>
2026-07-18 16:21:51 +0800

    feat: add settings normalization and cycle duration

--- tests/engine.test.js
+++ tests/engine.test.js
@@ -22,3 +22,23 @@ const { buildCycle } = require('../src/engine.js');
 })();
 
 console.log('engine buildCycle: all tests passed');
+
+const { normalizeSettings, cycleDuration } = require('../src/engine.js');
+
+// 夾限與預設補值
+(() => {
+  const s = normalizeSettings({ inhale: 0, holdIn: -3, exhale: 999, holdOut: 4, minutes: 0 });
+  assert.deepStrictEqual(s, { inhale: 1, holdIn: 0, exhale: 60, holdOut: 4, minutes: 1 }, 'clamp');
+})();
+(() => {
+  const s = normalizeSettings({});
+  assert.deepStrictEqual(s, { inhale: 2, holdIn: 2, exhale: 2, holdOut: 2, minutes: 5 }, 'defaults');
+})();
+
+// 一輪秒數
+(() => {
+  assert.strictEqual(cycleDuration({ inhale: 4, holdIn: 4, exhale: 4, holdOut: 4 }), 16, 'cycle 16s');
+  assert.strictEqual(cycleDuration({ inhale: 2, holdIn: 0, exhale: 2, holdOut: 0 }), 4, 'cycle 4s');
+})();
+
+console.log('engine settings: all tests passed');
